9a3872e663163e853236b7fd273890d8b850c6b9
Kal.V <kvaughn@skillstorm.com>
2021-12-09 16:14:13 -0500

    changed webserver

--- Pipeline-master/Instance Setup/instance.sh
+++ Pipeline-master/Instance Setup/instance.sh
@@ -6,10 +6,10 @@ aws ec2 run-instances \
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Jenkins}]'
 
 aws ec2 run-instances \
---image-id ami-0629230e074c580f2 \
---security-group-ids sg-02832c58304b8c2fd \
+--image-id ami-01216a3493f3ae184 \
+--security-group-ids sg-0e20b3038e5d6213f \
 --instance-type t2.medium \
---key-name OhioKeyPair \
+--key-name CaliforniaKeyPair \
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=WebServer}]'
 
 aws ec2 run-instances \
